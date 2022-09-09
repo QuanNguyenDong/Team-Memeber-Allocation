f23ac5457d49fcc7ceeb710b416015390da1b504
QuanNguyenDong <nguyendongquan031102@gmail.com>
2022-09-09 22:10:56 +1000

    Initial commit

---
+++ my-app/src/TeamMemberCard.jsx
@@ -0,0 +1,36 @@
+import femaleProfile from "./images/femaleProfile.jpg";
+import maleProfile from "./images/maleProfile.jpg";
+
+const TeamMemberCard = ({
+  employee,
+  handleEmployeeCardClick,
+  selectedTeam,
+}) => {
+  return (
+    <div
+      key={employee.id}
+      id={employee.id}
+      className={
+        employee.teamName === selectedTeam ? "card m-2 standout" : "card m-2"
+      }
+      style={{ cursor: "pointer" }}
+      onClick={handleEmployeeCardClick}
+    >
+      {employee.gender === "male" ? (
+        <img className="card-img-top" src={maleProfile} alt="" />
+      ) : (
+        <img className="card-img-top" src={femaleProfile} alt="" />
+      )}
+
+      <div className="card-body">
+        <h5 className="card-title">Full name: {employee.fullName}</h5>
+        <p className="card-text">
+          <b>Designation: </b>
+          {employee.designation}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TeamMemberCard;
